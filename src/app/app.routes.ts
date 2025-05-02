6bda4c0bbbbdee37ca57cbcd9b2d6f4c620d629e
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-01 19:39:48 -0500

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'meetings', component: MeetingsComponent },
+
+];
+
