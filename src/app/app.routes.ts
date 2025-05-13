85083f8f57522f2742bad85e2579fd4d74b45927
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-05-12 23:05:36 -0500

    feat(app): add the route to the product list component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
+import {ProductListComponent} from './products/pages/product-list/product-list.component';
+//import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
 
 export const routes: Routes = [
 
-  { path: 'meetings', component: MeetingsComponent },
+ // { path: 'meetings', component: MeetingsComponent },
+  {path: 'products',component: ProductListComponent}
 
 ];
 
