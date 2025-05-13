2874661e84ed1ed9b27fb86e791ee4642a346327
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-05-13 15:54:29 -0500

    fear(app-routes): add the product detail path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from './products/pages/product-list/product-list.component';
+import {ProductDetailComponent} from './products/components/product-detail/product-detail.component';
 //import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
 
 export const routes: Routes = [
 
  // { path: 'meetings', component: MeetingsComponent },
-  {path: 'products',component: ProductListComponent}
+  {path: 'products',component: ProductListComponent},
+  {path: 'product-detail/:id', component: ProductDetailComponent},
 
 ];
 
