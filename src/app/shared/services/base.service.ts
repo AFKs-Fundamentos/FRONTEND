5211212c7a9edb7ab07b21314f6f4896a0b2f43a
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-14 13:56:07 -0500

    feat: add getters by id for shared service

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -72,6 +72,16 @@ export class BaseService<T> {
       .pipe(retry(2), catchError(this.handleError));
   }
 
+  getById(id: any): Observable<T> {
+    return this.http.get<T>(`${this.resourcePath()}/${id}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  getAllById(id: any): Observable<T[]> {
+    return this.http.get<T[]>(`${this.resourcePath()}/${id}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
   private resourcePath(): string {
     return `${this.basePath}${this.resourceEndpoint}`;
   }
