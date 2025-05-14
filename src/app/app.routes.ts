fd2ad4e8d7584491e2dd37237658b548f7702bbe
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-05-14 10:17:41 -0500

    feat(app-routes): add path product-inventory.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from './products/pages/product-list/product-list.component';
 import {ProductDetailComponent} from './products/components/product-detail/product-detail.component';
+import {ProductInventoryComponent} from './products/pages/product-inventory/product-inventory.component';
 //import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
 
 export const routes: Routes = [
@@ -8,6 +9,7 @@ export const routes: Routes = [
  // { path: 'meetings', component: MeetingsComponent },
   {path: 'products',component: ProductListComponent},
   {path: 'product-detail/:id', component: ProductDetailComponent},
+  {path: 'product-inventory',component: ProductInventoryComponent},
 
 ];
 
