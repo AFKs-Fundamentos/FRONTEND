e802b43c134d5995688f080bdce69bfda723514f
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-22 16:37:26 -0500

    style: exchange select button instead of a common select

--- src/app/iam/pages/sign-up/sign-up.component.ts
+++ src/app/iam/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BaseFormComponent} from "../../../shared/components/base-form.component";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthenticationService} from "../../services/authentication.service";
 import {SignUpRequest} from "../../model/sign-up.request";
 import {NgIf} from "@angular/common";
@@ -10,22 +10,34 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { MultiSelectModule } from 'primeng/multiselect';
+import {InputGroup} from 'primeng/inputgroup';
+import {InputGroupAddon} from 'primeng/inputgroupaddon';
+import { PasswordModule } from 'primeng/password';
+import {SelectButton} from 'primeng/selectbutton';
+
+
+
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    DividerModule, ButtonModule, InputTextModule,SelectModule, MultiSelectModule,
+    DividerModule, ButtonModule, InputTextModule, SelectModule, MultiSelectModule, InputGroup, InputGroupAddon,
+    ReactiveFormsModule, PasswordModule, SelectButton, FormsModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent extends BaseFormComponent implements OnInit {
-  roles: any[] | undefined;
+  roles: any[] = [
+    { value: 'ROLE_TECHNICIAN', label: 'TÉCNICO' },
+    { value: 'ROLE_CLIENT', label: 'CLIENTE' },
+  ]
+
   form!: FormGroup;
   submitted = false;
-
+  stateOptions: any[] = [{ label: 'One-Way', value: 'one-way' },{ label: 'Return', value: 'return' }];
   selectedRole: any;
   constructor(private builder: FormBuilder, private authenticationService: AuthenticationService,
     private router: Router) {
@@ -33,10 +45,6 @@ export class SignUpComponent extends BaseFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.roles = [
-      { value: 'ROLE_TECHNICIAN', label: 'TÉCNICO' },
-      { value: 'ROLE_CLIENT', label: 'CLIENTE' },
-    ];
 
     this.form = this.builder.group({
       username: ['', Validators.required],
@@ -53,11 +61,13 @@ export class SignUpComponent extends BaseFormComponent implements OnInit {
 
     const signUpRequest = new SignUpRequest(username, password,selectedRole);
     this.authenticationService.signUp(signUpRequest);
-    this.submitted = true;
 
+    this.submitted = true;
+    this.router.navigate(['/sign-in']);
   }
 
   navigateToSignIn() {
         this.router.navigate(['/sign-in']);
       }
+
 }
