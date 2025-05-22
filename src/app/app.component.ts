232417120bc025534bfcdd979b405bc3117d008a
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-22 14:53:55 -0500

    feat:  add if for showing the sidebar when a user is signed in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {SidebarComponent} from './public/sidebar/sidebar.component';
 import { AuthenticationSectionComponent } from "./iam/components/authentication-section/authentication-section.component";
+import {AuthenticationService} from './iam/services/authentication.service';
+import {Drawer} from 'primeng/drawer';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, AuthenticationSectionComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PCMASTER';
+
+  isSignedIn: boolean = false;
+
+  constructor( private authenticationService: AuthenticationService) {
+
+    this.authenticationService.isSignedIn.subscribe(
+      (isSignedIn) => this.isSignedIn = isSignedIn
+    );
+  }
+
 }
