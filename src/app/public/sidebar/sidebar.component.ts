b13e7cc1e05eba9d5174d437288f97e06af9f694
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-22 14:56:36 -0500

    feat: add button to collapse sidebar

--- src/app/public/sidebar/sidebar.component.ts
+++ src/app/public/sidebar/sidebar.component.ts
@@ -5,7 +5,6 @@ import {Router, RouterLink} from '@angular/router';
 import {Menu} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
 import {Ripple} from 'primeng/ripple';
-import { AuthenticationSectionComponent } from "../../iam/components/authentication-section/authentication-section.component";
 import {AuthenticationService} from "../../iam/services/authentication.service";
 
 @Component({
@@ -15,13 +14,12 @@ import {AuthenticationService} from "../../iam/services/authentication.service";
   standalone: true,
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent implements OnInit{
-
+export class SidebarComponent  implements  OnInit {
   currentUserName: string = '';
   role: string = '';
   currentUserRole: string = '';
   isSignedIn: boolean = false;
-
+  collapsed = false;
   constructor(private authenticationService: AuthenticationService) {
     this.authenticationService.currentUsername.subscribe(
         (username) => this.currentUserName = username
@@ -42,66 +40,16 @@ export class SidebarComponent implements OnInit{
     this.currentUserRole=="ROLE_TECHNICIAN" ? this.role = "TÉCNICO" : this.role = "CLIENTE";
 
     this.items = [
-      {
-        separator: true
-      },
-      {
-        label: 'General',
-        items: [
-          {
-            label: 'Meetings',
-            icon: 'pi pi-video',
-            path: '/meetings'
-          },
-          {
-            label: 'Service History',
-            icon: 'pi pi-history',
-            path: 'service-history'
-          },
-          {
-            label: 'Ratings',
-            icon: 'pi pi-star',
-            path: 'ratings'
-          },
-          {
-            label: 'Products',
-            icon: 'pi pi-desktop',
-            path: '/products'
-          },
-          {
-            label: 'My Buys',
-            icon: 'pi pi-shopping-cart',
-            path: '/buys'
-          },
-
-        ]
-      },
-      {
-        label: 'Profile',
-        items: [
-          {
-            label: 'Settings',
-            icon: 'pi pi-cog',
-            path: '/'
-          },
-          {
-            label: 'Help',
-            icon: 'pi pi-inbox',
-            path: '/'
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            path: '/',
-            command: () => {
-              this.onSignOut();
-            }
-          }
-        ]
-      },
-      {
-        separator: true
-      }
+      { separator: true },
+      { label: 'Meetings', icon: 'pi pi-video', path: '/meetings' },
+      { label: 'Service History', icon: 'pi pi-history', path: '/service-history' },
+      { label: 'Ratings', icon: 'pi pi-star', path: '/ratings' },
+      { label: 'Products', icon: 'pi pi-desktop', path: '/products' },
+      { label: 'My Buys', icon: 'pi pi-shopping-cart', path: '/buys' },
+      { separator: true },
+      { label: 'Settings', icon: 'pi pi-cog', path: '/settings' },
+      { label: 'Help', icon: 'pi pi-inbox', path: '/help' },
+      { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.onSignOut() }
     ];
   }
 
@@ -110,8 +58,8 @@ export class SidebarComponent implements OnInit{
     // Sign out the user.
     this.authenticationService.signOut();
   }
-
-
-
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
+  }
 
 }
