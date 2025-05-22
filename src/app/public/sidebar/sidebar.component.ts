e0115c4c3ca611ebe0825a6d648a339d756da2fe
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-22 02:34:06 -0500

    feat: add dynamic username and role

--- src/app/public/sidebar/sidebar.component.ts
+++ src/app/public/sidebar/sidebar.component.ts
@@ -1,24 +1,46 @@
 import {Component, OnInit} from '@angular/core';
 import {AvatarModule} from 'primeng/avatar';
 import {ButtonModule} from 'primeng/button';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {Menu} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
 import {Ripple} from 'primeng/ripple';
 import { AuthenticationSectionComponent } from "../../iam/components/authentication-section/authentication-section.component";
+import {AuthenticationService} from "../../iam/services/authentication.service";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AvatarModule, ButtonModule, RouterLink, Menu, Ripple,AuthenticationSectionComponent],
+  imports: [AvatarModule, ButtonModule, RouterLink, Menu, Ripple],
   templateUrl: './sidebar.component.html',
   standalone: true,
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent implements OnInit{
 
+  currentUserName: string = '';
+  role: string = '';
+  currentUserRole: string = '';
+  isSignedIn: boolean = false;
+
+  constructor(private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUsername.subscribe(
+        (username) => this.currentUserName = username
+    );
+    this.authenticationService.isSignedIn.subscribe(
+        (isSignedIn) => this.isSignedIn = isSignedIn
+    );
+    this.authenticationService.currentUserRole.subscribe(
+        (currentUserRole) => this.currentUserRole = currentUserRole,
+    )
+  }
+
   items: MenuItem[] | undefined;
 
+
   ngOnInit() {
+
+    this.currentUserRole=="ROLE_TECHNICIAN" ? this.role = "TÉCNICO" : this.role = "CLIENTE";
+
     this.items = [
       {
         separator: true
@@ -70,7 +92,10 @@ export class SidebarComponent implements OnInit{
           {
             label: 'Logout',
             icon: 'pi pi-sign-out',
-            path: '/'
+            path: '/',
+            command: () => {
+              this.onSignOut();
+            }
           }
         ]
       },
@@ -79,4 +104,14 @@ export class SidebarComponent implements OnInit{
       }
     ];
   }
+
+
+  onSignOut() {
+    // Sign out the user.
+    this.authenticationService.signOut();
+  }
+
+
+
+
 }
