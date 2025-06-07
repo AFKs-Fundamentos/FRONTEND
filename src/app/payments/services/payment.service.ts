abed34784e3fce2450a83970160a78e76c170ad0
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2025-06-06 19:49:28 -0500

    feat(payments): payment confirm service

---
+++ src/app/payments/services/payment.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from '../../shared/services/base.service';
+import {HttpClient} from '@angular/common/http';
+import {catchError, Observable, retry} from 'rxjs';
+import { Payment } from '../model/payment.entity';
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService extends BaseService<Payment>{
+
+  constructor(http: HttpClient) {
+        super(http);
+        this.resourceEndpoint = '/payments';
+  }
+
+  confirm(id:string): Observable<string> {
+    return this.http.post<string>(`${this.resourcePath()}/confirm/${id}`)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+}
