227f67ea1bfc099ccff472ee19962c3c9406fde7
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2025-06-06 19:24:21 -0500

    rebase(fixing): deleted duplicated of provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import {MyPreset} from './mypreset';
 
