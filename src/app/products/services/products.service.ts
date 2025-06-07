c7c34c49a10f54a1ff43c96d4ff299c286c45ae7
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2025-06-06 19:23:12 -0500

    feat(fixing): override implemented in products

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -7,14 +7,14 @@ import {catchError, Observable, retry} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService extends BaseService<Product>
-{
+export class ProductsService extends BaseService<Product> {
   constructor(http: HttpClient ) {
     super(http);
     this.resourceEndpoint = '/products';
   }
 
-  getById(id: number): Observable<Product> {
+
+  override getById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.basePath}${this.resourceEndpoint}?id=${id}`, this.httpOptions)
       .pipe(retry(2),catchError(this.handleError));
   }
