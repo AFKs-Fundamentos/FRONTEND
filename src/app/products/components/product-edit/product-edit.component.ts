d6ca0988f1f5975c9e2330ae8c6279d876e405b9
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 10:27:52 -0500

    feat(product-edit): add a component for editing products with a form, validation, and visual feedback.

---
+++ src/app/products/components/product-edit/product-edit.component.ts
@@ -0,0 +1,83 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../model/product.entity';
+import { MessageService } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextarea } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {Card} from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+
+@Component({
+  selector: 'app-product-edit',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputNumberModule,
+    InputTextModule,
+    InputTextarea,
+    ButtonModule,
+    Card,
+    FloatLabelModule
+  ],
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent {
+  @Input() product!: Product;
+  @Output() save = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  loading: boolean = false;
+
+  constructor(
+    private productService: ProductsService,
+    private messageService: MessageService
+  ) {}
+
+  onSubmit(form: NgForm): void {
+    if (form.invalid) {
+      this.markFormControlsAsTouched(form);
+      return;
+    }
+
+    this.loading = true;
+
+    const productData = { ...this.product };
+
+    this.productService.update(this.product.id, productData).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Producto actualizado correctamente'
+        });
+        this.save.emit();
+      },
+      error: (err) => {
+        console.error('Error al actualizar el producto:', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo actualizar el producto'
+        });
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    });
+  }
+  private markFormControlsAsTouched(form: NgForm): void {
+    Object.keys(form.controls).forEach(key => {
+      form.controls[key].markAsTouched();
+    });
+  }
+  onCancel(): void {
+    this.cancel.emit();
+  }
+}
