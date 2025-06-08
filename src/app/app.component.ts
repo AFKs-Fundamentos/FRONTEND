0ee6dc3a09ac0ab8c93dc933a60581ee65b07674
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 10:49:01 -0500

    feat(app): add path and imports.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SidebarComponent} from './public/sidebar/sidebar.component';
+import {Button} from 'primeng/button';
+import {ProductInventoryComponent} from './products/pages/product-inventory/product-inventory.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, Button, ProductInventoryComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import {ProductListComponent} from './products/pages/product-list/product-list.component';
 import {ProductDetailComponent} from './products/components/product-detail/product-detail.component';
 import {ProductInventoryComponent} from './products/pages/product-inventory/product-inventory.component';
-import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
+//import {MeetingsComponent} from './meetings/components/meetings/meetings.component';
 import {ServiceHistoryComponent} from './meetings/components/service-history/service-history.component';
 import {RatingCardComponent} from './meetings/components/rating-card/rating-card.component';
+import {ShoppingCartComponent} from './shopping_cart/pages/shopping-cart/shopping-cart.component';
+
 
 export const routes: Routes = [
 
  // { path: 'meetings', component: MeetingsComponent },
   {path: 'products',component: ProductListComponent},
   {path: 'product-detail/:id', component: ProductDetailComponent},
+  {path: 'shopping-cart/:user_client_id', component: ShoppingCartComponent},
   {path: 'product-inventory',component: ProductInventoryComponent},
   {path: 'service-history', component: ServiceHistoryComponent },
   {path: 'ratings', component: RatingCardComponent },
