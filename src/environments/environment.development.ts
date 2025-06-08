790219ee44365b7799774157af48ed49ad0ceae1
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 05:50:20 -0500

    feat(environments): add new server base path.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:3001/api/v1'
+  //serverBasePath: 'http://localhost:3001/api/v1'
+  serverBasePath: 'http://localhost:3001'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: true,
-  serverBasePath: 'http://localhost:3001/api/v1'
+  //serverBasePath: 'http://localhost:3001/api/v1'
+  serverBasePath: 'http://localhost:3001'
 };
 
