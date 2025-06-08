aeb74a1737fe3454575e02bb2cbbecb3379db293
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 10:48:06 -0500

    feat(product-list): add required imports and services for product list and cart integration

--- src/app/products/pages/product-list/product-list.component.ts
+++ src/app/products/pages/product-list/product-list.component.ts
@@ -6,18 +6,28 @@ import {Product} from '../../model/product.entity';
 import {CurrencyPipe, NgForOf} from '@angular/common';
 import {ButtonModule,Button} from 'primeng/button';
 import {Router} from '@angular/router';
+import {ShoppingCartService} from '../../../shopping_cart/services/shopping-cart.service';
+import {ShoppingCart} from '../../../shopping_cart/model/shopping-cart.entity';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule, CardModule, NgForOf, CurrencyPipe, Button, ButtonModule],
+  providers: [MessageService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit{
+  userClientId: number = 100;
   productData: Product[] = [];
 
-  constructor(private productService: ProductsService, private router: Router) {}
+  constructor(
+    private productService: ProductsService,
+    private shoppingCartService: ShoppingCartService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.loadProducts();
@@ -33,19 +43,38 @@ export class ProductListComponent implements OnInit{
       }
     );
   }
-  toggleFavorite(product: Product): void {
-    product.isFavorite = !product.isFavorite;
-  }
+
   addToCart(product: Product): void {
-    const productId = product.id;
-    //console.log('Producto añadido al carrito con ID:', productId);
-    // Aqui debajo llamas al servicio de carrito el que lo este haciendo ;)
+    const newCartItem: ShoppingCart = {
+      user_client_id: this.userClientId,
+      product_id: product.id,
+      product_name: product.productName,
+      product_price: product.price,
+      product_quantity: 1, // Cantidad inicial siempre 1
+      status_shopping_cart_item: 'PENDING' // Estado inicial
+    };
+
+    this.shoppingCartService.create(newCartItem).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Producto añadido al carrito'
+        });
+      },
+      error: (err) => {
+        console.error('Error adding to cart:', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo añadir al carrito'
+        });
+      }
+    });
+    this.router.navigate(['/shopping-cart', this.userClientId]);
   }
   seeDetail(product: Product): void {
-
     const productId = product.id;
     this.router.navigate(['/product-detail', productId]);
-
-
   }
 }
