b74e4b6f21bfc89f2c018e931fc2fcbd30071b24
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 05:53:35 -0500

    feat(products-model): adds new inventory attribute.

--- src/app/products/model/inventory.entity.ts
+++ src/app/products/model/inventory.entity.ts
@@ -1,13 +1,18 @@
 export class Inventory {
-  id: number;
-  stock: number;
+  id?: number;
+  user_technical_id: number;
   product_id: number;
-  user_id: number;
+  stock: number;
+  stock_min: number;
+  stock_max: number;
+
   constructor() {
     this.id = 0;
-    this.stock = 0;
+    this.user_technical_id = 0;
     this.product_id = 0;
-    this.user_id = 0;
+    this.stock = 0;
+    this.stock_min = 0;
+    this.stock_max = 0;
   }
 }
 
