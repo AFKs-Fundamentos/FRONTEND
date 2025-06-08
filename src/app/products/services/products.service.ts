fdaf111e380aec6f104f61b8948ba709e2e2137d
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-08 10:37:58 -0500

    feat(product-service): update the name method get.

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -14,7 +14,7 @@ export class ProductsService extends BaseService<Product>
     this.resourceEndpoint = '/products';
   }
 
-  getById(id: number): Observable<Product> {
+  getProductById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.basePath}${this.resourceEndpoint}?id=${id}`, this.httpOptions)
       .pipe(retry(2),catchError(this.handleError));
   }
