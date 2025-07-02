d57f2a60ea329b1a7d849e0f1ce7141de410caff
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-01 23:48:00 -0500

    feat: add schedule entity

--- src/app/meetings/model/advisor.entity.ts
+++
@@ -1,7 +0,0 @@
-export interface Advisor {
-  id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  phone: string;
-}
--- src/app/meetings/services/advisor.service.ts
+++ src/app/meetings/services/advisor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Advisor} from '../model/advisor.entity';
 import {BaseService} from '../../shared/services/base.service';
 import {HttpClient} from '@angular/common/http';
+import {Advisor} from '../../profiles/model/advisor.entity';
 
 
 @Injectable()
