a6958cc30fb6dbfabafeb038c3cc83d91758ef2c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-01 23:52:36 -0500

    chore: update get request for scheduling

--- src/app/profiles/services/scheduling.service.ts
+++ src/app/profiles/services/scheduling.service.ts
@@ -1,20 +1,25 @@
-import {Injectable} from '@angular/core';
-import { BaseService } from "../../shared/services/base.service";
-import { HttpClient } from "@angular/common/http";
-import {catchError, Observable, retry} from 'rxjs';
-import {Schedule} from '../model/schedule.entity';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, retry, catchError } from 'rxjs';
+import { BaseService } from '../../shared/services/base.service';
+import { AdvisorSchedule } from '../../meetings/model/advisorSchedule.entity';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SchedulingService extends BaseService<Schedule> {
-  constructor(http: HttpClient ) {
+export class SchedulingService extends BaseService<AdvisorSchedule> {
+  constructor(http: HttpClient) {
     super(http);
     this.resourceEndpoint = '/schedules';
   }
 
-  getProductById(id: number): Observable<Schedule> {
-    return this.http.get<Schedule>(`${this.basePath}${this.resourceEndpoint}?id=${id}`, this.httpOptions)
-      .pipe(retry(2),catchError(this.handleError));
+  getScheduleByAdvisorId(advisorId: number): Observable<AdvisorSchedule[]> {
+    return this.http.get<AdvisorSchedule[]>(
+      `${this.basePath}${this.resourceEndpoint}/advisor/${advisorId}`,
+      this.httpOptions
+    ).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 }
