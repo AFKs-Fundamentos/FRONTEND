7e6017ff02a96876a15cb78730a7b1261ffcbd34
diego5m <diegosoriano137@gmail.com>
2025-07-06 00:12:29 -0500

    feat: Update Ratings and Add Shipping

--- src/app/ratings/components/rating-form/rating-form.component.ts
+++ src/app/ratings/components/rating-form/rating-form.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { Rating } from 'primeng/rating';
+import { TextareaModule } from 'primeng/textarea';
+import { RatingsService } from '../../services/ratings.service';
+
+import { AdvisoryRating, ProductRating, UserRating } from '../../model/rating.model';
+import { AuthenticationService } from '../../../iam/services/authentication.service';
+
 
 @Component({
   selector: 'app-rating-form',
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, FormsModule, TextareaModule, ButtonModule, Rating],
   templateUrl: './rating-form.component.html',
   styleUrl: './rating-form.component.css'
 })
 export class RatingFormComponent {
+
+  ratingForm: FormGroup;
+  
+  rating: any
+
+  @Input() id: number;
+  @Input() context: string;
+  
+  constructor(private fb: FormBuilder, private ratingService: RatingsService, private auth: AuthenticationService){
+    this.ratingForm = this.fb.group({
+      punctuation: [0, Validators.required],
+      description: ['', [Validators.required, Validators.minLength(5)]],
+      userId: this.auth.getCurrentUserId
+    })
+
+    this.id = 0;
+    this.context = '';
+  }
+
+  CreateRating(): void{
+
+    if(this.ratingForm.valid){
+      
+      switch(this.context){
+        case 'product': {
+          
+          const formValue = this.ratingForm.value;
+
+          const ratingP: ProductRating = {
+            ...formValue,
+            productId: this.id,
+          };
+
+          this.rating = ratingP
+
+          break;
+        
+        }
+        case 'advisory': {
+
+          const formValue = this.ratingForm.value;
+
+          const ratingA: AdvisoryRating = {
+            ...formValue,
+            advisoryId: this.id,
+          };
+
+          this.rating = ratingA
+
+          break;
+        }
+        case 'user': {
+
+          const formValue = this.ratingForm.value;
+
+          const ratingU: UserRating = {
+            ...formValue,
+            technicalId: this.id,
+          };
+
+          this.rating = ratingU
+
+          break;
+        }
+      }
+
+      this.ratingService.createRating(this.rating, this.context).subscribe({
+        next: (res) => {
+          console.log('Rating creado:', res);
+          this.ratingForm.reset({ punctuation: 0, description: '' });
+        },
+        error: (err) => {
+          console.error('Error al crear rating:', err);
+        }
+      })
+
+    } else {console.log('Rating invalido'), this.ratingForm.reset({ punctuation: 0, description: '' })}
+  }
 
 }
---
+++ src/app/ratings/components/ratings-by-context/ratings-by-context.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { RatingsService } from '../../services/ratings.service';
+import { RatingCardComponent } from '../rating-card/rating-card.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ratings-by-context',
+  imports: [RatingCardComponent, CommonModule],
+  templateUrl: './ratings-by-context.component.html',
+  styleUrl: './ratings-by-context.component.css'
+})
+export class RatingsByContextComponent implements OnInit{
+
+  ratingsByType: any[];
+  @Input() context: string;
+  @Input() contextId: number;
+
+  constructor(private ratingServie: RatingsService){
+    this.ratingsByType = [];
+    this.context = '';
+    this.contextId = -1;
+  }
+
+  ngOnInit(): void {
+
+    if(this.context === 'user'){
+
+      this.ratingServie.getRatingsByTechnical(this.context, this.contextId).subscribe((response: any)=> {
+      this.ratingsByType = response;
+      console.log("ratings data:", this.ratingsByType);
+    })
+
+    }else {
+
+      this.ratingServie.getRatingsByType(this.context, this.contextId).subscribe((response: any)=> {
+      this.ratingsByType = response;
+      console.log("ratings data:", this.ratingsByType)
+    })
+
+    }
+
+  }
+
+}
