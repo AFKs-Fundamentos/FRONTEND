83dccfb0b3f32e49dd5387c820219b1b15b8fe4e
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-07-05 19:59:58 -0500

    feat(app-components): remove unused ProductInventoryComponent import
    Eliminated the import and usage of ProductInventoryComponent from app.component.ts as it is no longer needed in the imports array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {SidebarComponent} from './public/sidebar/sidebar.component';
 import {Button} from 'primeng/button';
-import {ProductInventoryComponent} from './products/pages/product-inventory/product-inventory.component';
 import { AuthenticationSectionComponent } from "./iam/components/authentication-section/authentication-section.component";
 import {AuthenticationService} from './iam/services/authentication.service';
 import {Drawer} from 'primeng/drawer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, Button, ProductInventoryComponent],
+  imports: [RouterOutlet, SidebarComponent, Button],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
