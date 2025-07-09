3801b0a3aa7557cb6da34869ba5aedc5ef0494ab
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-07-08 20:14:31 -0500

    feat(product-list): add  filter by category product and search.

--- src/app/products/pages/product-list/product-list.component.ts
+++ src/app/products/pages/product-list/product-list.component.ts
@@ -12,19 +12,27 @@ import {ProductItemService} from '../../../shopping_cart/services/product-item.s
 import {ShoppingCartService} from '../../../shopping_cart/services/shopping-cart.service';
 import {AuthenticationService} from '../../../iam/services/authentication.service';
 import {Tooltip} from 'primeng/tooltip';
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, CardModule, NgForOf, CurrencyPipe, Button, ButtonModule, Tooltip],
+  imports: [CommonModule, CardModule, NgForOf, CurrencyPipe, Button, ButtonModule, Tooltip, DropdownModule, FormsModule, InputTextModule],
   providers: [MessageService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit{
   productData: Product[] = [];
+  filteredProducts: Product[] = [];
   userId: number = 0;
   currentUserRole: string = '';
+
+  searchTerm: string = '';
+  selectedCategory: string = '';
+  categories: string[] = [];
 
   constructor(
     private productService: ProductsService,
@@ -44,15 +52,31 @@ export class ProductListComponent implements OnInit{
     this.loadProducts();
   }
   loadProducts() {
-    this.productService.getAll().subscribe(
+    this.productService.getProductsByStock().subscribe(
       (data: Product[]) => {
         this.productData = data;
+        this.filteredProducts = [...this.productData];
+        this.categories = [...new Set(this.productData.map(product => product.category))];
         console.log(this.productData);
       },
       (error: any) => {
         console.error('Error fetching products', error);
       }
     );
+  }
+  filterProducts() {
+    this.filteredProducts = this.productData.filter(product => {
+      // Filtro por término de búsqueda
+      const matchesSearch = this.searchTerm === '' ||
+          product.productName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          product.category.toLowerCase().includes(this.searchTerm.toLowerCase());
+
+      // Filtro por categoría
+      const matchesCategory = !this.selectedCategory ||
+        product.category === this.selectedCategory;
+
+      return matchesSearch && matchesCategory;
+    });
   }
 
   addToCart(product: Product): void {
